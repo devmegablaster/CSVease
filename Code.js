ae0a96ec6f04e4fa0333a27f92adec613bd99a22
Ojas Tyagi <devmegablaster@gmail.com>
2023-08-04 13:09:08 +0530

    feat: import csv from file

--- Code.js
+++ Code.js
@@ -4,7 +4,7 @@ function onOpen() {
   // Create a menu and add options to it
   ui.createMenu("CSVease")
     .addItem("Import CSV from Link", "importCSVFromLink")
-    .addItem("Import CSV from File", "importCSVFromLink")
+    .addItem("Import CSV from File", "importCSVFromFile")
     .addToUi()
 }
 
@@ -53,6 +53,44 @@ function createGraphFromData(csvData) {
     barChart: barChart.getBlob(),
     areaChart: areaChart.getBlob()
   }
+}
+
+function importCsvFromText(text) {
+  const ui = SpreadsheetApp.getUi()
+  const sheet = SpreadsheetApp.getActiveSheet()
+
+  const prevDataColumns = sheet.getLastColumn()
+
+  // Parsing the CSV text
+  const csv = text
+  const data = Utilities.parseCsv(csv)
+
+  // Check if the structure of the CSV file matches the structure of the sheet
+  if (prevDataColumns > 0 && prevDataColumns != data[0].length) {
+    const response = ui.alert("Number of columns in CSV file does not match the number of columns in the sheet. Do you want to continue?", ui.ButtonSet.YES_NO)
+
+    if (response == ui.Button.NO) {
+      ui.alert("Import cancelled!")
+      return
+    }
+  }
+
+  // Populate sheet with data
+  const range = sheet.getRange(sheet.getLastRow() + 1, 1, data.length, data[0].length)
+  range.setValues(data)
+
+  // Show success message and ask for email if user wants to receive CSV analytics
+  const response = ui.alert("Import successful, do you want to receive a mail for CSV analytics?", ui.ButtonSet.YES_NO)
+
+  if (response == ui.Button.YES) {
+    const email = ui.prompt("Enter your email").getResponseText()
+    sendEmail(email, "CSV Analytics - CSVease", `<h2>Here are the analytics for your CSV File</h2><img src="cid:barChart"><img src="cid:areaChart">`, createGraphFromData(data))
+  }
+}
+
+function importCSVFromFile() {
+  const html = HtmlService.createHtmlOutputFromFile("index")
+  SpreadsheetApp.getUi().showModalDialog(html, "Import CSV from File")
 }
 
 function importCSVFromLink() {
